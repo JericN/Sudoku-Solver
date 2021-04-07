d8cafeb2580845fb987013cde4b3d44e5e439234
Jeric <jericnarte912@gmail.com>
2021-04-08 03:28:26 +0800

    Update 0.6
    Working Interface

--- src/Logic.java
+++ src/Logic.java
@@ -1,2 +1,26 @@
 public class Logic {
+    Variable var = new Variable();
+    int[][] cell;
+
+    public void start() {
+        cell = var.getCell();
+        solve();
+    }
+
+    public void solve() {
+        int[][] flagVals = new int[9][9];
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                if(cell[y][x]==0){
+                    for (int i = 1; i < 10; i++) {
+                        for (int k = 0; k < 9; k++) {
+                            if(cell[y][k] == i){
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -1,2 +1,12 @@
 public class Variable {
+    int[][] cell;
+    public void value(){
+        cell = new int[9][9];
+    }
+    public void setCell(int[][] input){
+        cell=input;
+    }
+    public int[][] getCell(){
+        return cell;
+    }
 }
