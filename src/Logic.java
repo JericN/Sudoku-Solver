e6998fb6a4a9541303b05e33e96be06d9dc8357b
Jeric <jericnarte912@gmail.com>
2021-04-09 03:27:39 +0800

    update 0.75
    optimization

--- src/Logic.java
+++ src/Logic.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Logic {
     Variable var = new Variable();
+    int emptyCellCount = 0;
+    ArrayList<ArrayList<Integer>> emptyCell = new ArrayList<ArrayList<Integer>>();
     int[][][] flagVals;
     int[][] cell;
 
@@ -8,7 +14,8 @@ public class Logic {
         cell = var.getCell();
         flagVals = new int[9][9][9];
         getPossibleSolutions();
-        if (iterateSolutions(0, 0)) {
+        sortFlags();
+        if (iterateSolutions(0)) {
             System.out.println("Solution Found");
         } else {
             System.out.println("Invalid Sudoku");
@@ -36,7 +43,7 @@ public class Logic {
                             val++;
                         }
                     }
-                    flagVals[y][x][0] = val;
+                    flagVals[y][x][0] = val - 1;
                 }
             }
         }
@@ -44,29 +51,29 @@ public class Logic {
 
     static int count = 0;
 
-    public boolean iterateSolutions(int yPos, int xPos) {
-        for (int y = yPos; y < 9; y++) {
-            if(y!=yPos){
-                xPos=0;
-            }
-            for (int x = xPos; x < 9; x++) {
-                if (x == 8 && y == 8) {
-                    count++;
-                    System.out.println("DONE: " + count);
-                    return checkSolution();
-                }
-                if (cell[y][x] == 0) {
-                    for (int k = 1; k < flagVals[y][x][0]; k++) {
-                        System.out.println(y+" "+x);
-                        cell[y][x] = flagVals[y][x][k];
-                        if(iterateSolutions(y, x)){
-                            return true;
-                        }
+    public boolean iterateSolutions(int nPos) {
+        for (int i = nPos; i < emptyCellCount; i++) {
+            if (i == emptyCellCount-1) {
+                count++;
+                System.out.println("DONE: " + count);
+                for (int k = 0; k < 9; k++) {
+                    for (int j = 0; j < 9; j++) {
+                        System.out.print("  " + cell[k][j]);
                     }
-                    cell[y][x] = 0;
-                    return false;
+                    System.out.println();
+                }
+                return checkSolution();
+            }
+            int yPos = emptyCell.get(i).get(0);
+            int xPos = emptyCell.get(i).get(1);
+            for (int k = 0; k < emptyCell.get(i).get(2); k++) {
+                cell[yPos][xPos] = emptyCell.get(i).get(k + 3);
+                if (iterateSolutions(i + 1)) {
+                    return true;
                 }
             }
+            cell[yPos][xPos] = 0;
+            return false;
         }
         return false;
     }
@@ -92,12 +99,6 @@ public class Logic {
             }
         }
         if (flag) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    System.out.print("  " + cell[i][j]);
-                }
-                System.out.println();
-            }
             System.out.println("Returned True");
             return true;
         } else {
@@ -114,6 +115,42 @@ public class Logic {
                 }
                 System.out.println("\n\n");
             }
+        }
+    }
+
+
+    public void sortFlags() {
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
+                if (flagVals[y][x][0] != 0) {
+                    emptyCell.add(new ArrayList<Integer>());
+                    emptyCell.get(emptyCellCount).add(y);
+                    emptyCell.get(emptyCellCount).add(x);
+                    emptyCell.get(emptyCellCount).add(flagVals[y][x][0]);
+                    for (int j = 0; j < flagVals[y][x][0]; j++) {
+                        emptyCell.get(emptyCellCount).add(flagVals[y][x][j + 1]);
+                    }
+                    emptyCellCount++;
+                }
+            }
+        }
+
+        for (int i = 0; i < emptyCellCount; i++) {
+            ArrayList<Integer> key = (ArrayList<Integer>) emptyCell.get(i).clone();
+            int j = i - 1;
+            while (j >= 0 && emptyCell.get(j).get(2) > key.get(2)) {
+                emptyCell.get(j + 1).clear();
+                emptyCell.get(j + 1).addAll(emptyCell.get(j));
+                j = j - 1;
+            }
+            emptyCell.get(j + 1).clear();
+            emptyCell.get(j + 1).addAll(key);
+        }
+        for (int i = 0; i < emptyCellCount; i++) {
+            for (int j = 0; j < emptyCell.get(i).get(2)+3; j++) {
+                System.out.print(" "+emptyCell.get(i).get(j));
+            }
+            System.out.println();
         }
     }
 }
